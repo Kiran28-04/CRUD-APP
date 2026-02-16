40b1c1e3bbb01fabcbe5b08a0cfa84518d958f6b
Kiran Dashrath Kashid <kashidkiran393@gmail.com>
2026-02-16 08:21:23 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function(req,res,next){
-  const authHeader = req.headers.authorization;
-  if(!authHeader) return res.status(401).send("Access denied");
+  const header = req.headers.authorization;
+  if(!header) return res.status(401).send("Access Denied");
 
-  const token = authHeader.split(" ")[1];
+  const token = header.split(" ")[1];
+
   try{
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
     next();
   }catch{
-    res.status(401).send("Invalid token");
+    res.status(400).send("Invalid Token");
   }
 };
