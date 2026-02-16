f3b598dbdb35aa4d48d4d78ed7646d4e5dd225b1
Kiran Dashrath Kashid <kashidkiran393@gmail.com>
2026-02-16 13:47:28 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-  name:String,
-  email:{type:String,unique:true},
-  password:String
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+
+  // ADD THESE FOR RESET FEATURE
+  resetToken: String,
+  resetTokenExpire: Date
 });
 
-module.exports = mongoose.model("User",schema);
+module.exports = mongoose.model("User", schema);
