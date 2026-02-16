4fa2946c6969d4ae159dc6703f146ce27142d9e2
Kiran Dashrath Kashid <kashidkiran393@gmail.com>
2026-02-16 08:24:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,85 +1,93 @@
-const api = "http://localhost:3000";
-
 /* REGISTER */
-function register() {
-  fetch(`${api}/register`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      name: name.value,
-      email: email.value,
-      password: password.value
-    })
-  })
-  .then(res => res.text())
-  .then(msg => {
-    alert(msg);
-    if (msg === "Registration successful") {
-      window.location.href = "index.html";
-    }
-  });
+function register(){
+fetch(`${api}/register`,{
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({
+name:name.value,
+email:email.value,
+password:password.value
+})
+})
+.then(res=>res.text())
+.then(msg=>{
+alert(msg);
+if(msg==="Registration successful")
+window.location.href="index.html";
+});
 }
 
 /* LOGIN */
-function login() {
-  fetch(`${api}/login`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      email: loginEmail.value,
-      password: loginPassword.value
-    })
-  })
-  .then(res => res.json())
-  .then(data => {
-    if (data.token) {
-      localStorage.setItem("token", data.token);
-      localStorage.setItem("username", data.name);
-      window.location.href = "index.html";
-    } else {
-      alert("Login failed");
-    }
-  });
+function login(){
+fetch(`${api}/login`,{
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({
+email:loginEmail.value,
+password:loginPassword.value
+})
+})
+.then(res=>res.json())
+.then(data=>{
+if(data.token){
+localStorage.setItem("token",data.token);
+window.location.href="crud.html";
+}
+});
 }
 
-/* FORGOT PASSWORD */
-function sendResetLink() {
-  fetch(`${api}/reset-password`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      email: resetEmail.value
-    })
-  })
-  .then(res => res.text())
-  .then(msg => {
-    alert(msg);
-    window.location.href = "index.html";   // ✅ Redirect after sending link
-  });
+/* ADD BOOK */
+function addBook(){
+fetch(`${api}/books`,{
+method:"POST",
+headers:{
+"Content-Type":"application/json",
+"Authorization":"Bearer "+localStorage.getItem("token")
+},
+body:JSON.stringify({
+bookName:bookName.value,
+author:author.value,
+genre:genre.value,
+price:price.value
+})
+})
+.then(res=>res.text())
+.then(alert)
+.then(loadBooks);
 }
 
-/* RESET PASSWORD */
-function updatePassword() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const token = urlParams.get("token");
-
-  fetch(`${api}/update-password`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      token: token,
-      password: newPassword.value
-    })
-  })
-  .then(res => res.text())
-  .then(msg => {
-    alert(msg);
-    window.location.href = "index.html";  // ✅ Redirect after reset
-  });
+/* LOAD BOOKS */
+function loadBooks(){
+fetch(`${api}/books`,{
+headers:{
+"Authorization":"Bearer "+localStorage.getItem("token")
+}
+})
+.then(res=>res.json())
+.then(data=>{
+const list=document.getElementById("bookList");
+list.innerHTML="";
+data.forEach(b=>{
+list.innerHTML+=`
+<tr>
+<td>${b.bookName}</td>
+<td>${b.author}</td>
+<td>${b.genre}</td>
+<td>${b.price}</td>
+<td><button onclick="deleteBook('${b._id}')">Delete</button></td>
+</tr>
+`;
+});
+});
 }
 
-/* LOGOUT */
-function logout() {
-  localStorage.clear();
-  window.location.href = "index.html";
+/* DELETE */
+function deleteBook(id){
+fetch(`${api}/books/${id}`,{
+method:"DELETE",
+headers:{
+"Authorization":"Bearer "+localStorage.getItem("token")
+}
+})
+.then(loadBooks);
 }
