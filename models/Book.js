7ba5e075c000aa5910c3d6ae8b50b347b051a6d3
Kiran Dashrath Kashid <kashidkiran393@gmail.com>
2026-02-16 08:23:12 +0530

    Update Book.js

--- models/Book.js
+++ models/Book.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const BookSchema = new mongoose.Schema({
+const schema = new mongoose.Schema({
   bookName:String,
   author:String,
   genre:String,
   price:Number
 });
 
-module.exports = mongoose.model("Book",BookSchema);
+module.exports = mongoose.model("Book",schema);
